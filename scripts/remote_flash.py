0c170cbe189339467b0e362f9aee87d54ba776b2
Mark Rossington <mark@rossinbox.com>
2023-03-09 22:46:43 +0000

    base remote flash file

---
+++ scripts/remote_flash.py
@@ -0,0 +1,15 @@
+# Standard Includes
+import os
+import subprocess
+
+# Local Includes
+import settings
+from common import Common
+
+
+def main():
+    Common.work_top_level()
+
+
+if __name__ == "__main__":
+    main()
