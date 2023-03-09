e2e4754145ac35dae396df45531ed41320fc7713
Mark Rossington <mark@rossinbox.com>
2023-03-09 22:50:50 +0000

    add error checking to missing local assets

--- scripts/remote_flash.py
+++ scripts/remote_flash.py
@@ -19,9 +19,16 @@ def main():
     if os.path.isfile("output/firmware.bin"):
         print("[Info] Found firmware.bin, copying to remote host")
         Common.run_process(["scp", "-r", "output", f"{settings.remote_address}:{settings.remote_folder}/"])
+    else:
+        print("[Error] output/firmware.bin not found, check if it built correctly")
+        return
+
     if os.path.isdir("scripts"):
         print("[Info] Found scripts directory, copying to remote host")
         Common.run_process(["scp", "-r", "scripts", f"{settings.remote_address}:{settings.remote_folder}/"])
+    else:
+        print("[Error] scripts directory not found")
+        return
 
 if __name__ == "__main__":
     main()
