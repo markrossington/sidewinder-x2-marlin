0bd0a42beeb1964899f01d8f8133f39581be699f
Mark Rossington <mark@rossinbox.com>
2023-03-09 22:50:32 +0000

    delete existing remote folder and copy assets across if they exist

--- scripts/remote_flash.py
+++ scripts/remote_flash.py
@@ -10,6 +10,18 @@ from common import Common
 def main():
     Common.work_top_level()
 
+    print("[Info] Delete existing temporary folder for scripts and binary on remote host")
+    Common.run_process(["ssh", settings.remote_address, f'if [ -d "{settings.remote_folder}" ]; then rm -r {settings.remote_folder}; fi'])
+
+    print("[Info] Making temporary folder for scripts and binary on remote host")
+    Common.run_process(["ssh", settings.remote_address, f"mkdir {settings.remote_folder}"])
+
+    if os.path.isfile("output/firmware.bin"):
+        print("[Info] Found firmware.bin, copying to remote host")
+        Common.run_process(["scp", "-r", "output", f"{settings.remote_address}:{settings.remote_folder}/"])
+    if os.path.isdir("scripts"):
+        print("[Info] Found scripts directory, copying to remote host")
+        Common.run_process(["scp", "-r", "scripts", f"{settings.remote_address}:{settings.remote_folder}/"])
 
 if __name__ == "__main__":
     main()
