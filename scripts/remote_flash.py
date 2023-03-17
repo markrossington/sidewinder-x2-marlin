7ba19c6570c33c486d30713ce25b9a0ad9608c13
markrossington <124208527+markrossington@users.noreply.github.com>
2023-03-17 21:43:39 +0000

    add: remote flash script (#18)

---
+++ scripts/remote_flash.py
@@ -0,0 +1,37 @@
+# Standard Includes
+import os
+import subprocess
+
+# Local Includes
+import settings
+from common import Common
+
+
+def main():
+    Common.work_top_level()
+
+    print("[Info] Delete existing temporary folder for scripts and binary on remote host")
+    Common.run_process(["ssh", settings.remote_address, f'if [ -d "{settings.remote_folder}" ]; then rm -r {settings.remote_folder}; fi'])
+
+    print("[Info] Making temporary folder for scripts and binary on remote host")
+    Common.run_process(["ssh", settings.remote_address, f"mkdir {settings.remote_folder}"])
+
+    if os.path.isfile("output/firmware.bin"):
+        print("[Info] Found firmware.bin, copying to remote host")
+        Common.run_process(["scp", "-r", "output", f"{settings.remote_address}:{settings.remote_folder}/"])
+    else:
+        print("[Error] output/firmware.bin not found, check if it built correctly")
+        return
+
+    if os.path.isdir("scripts"):
+        print("[Info] Found scripts directory, copying to remote host")
+        Common.run_process(["scp", "-r", "scripts", f"{settings.remote_address}:{settings.remote_folder}/"])
+    else:
+        print("[Error] scripts directory not found")
+        return
+
+    subprocess.run(["ssh", settings.remote_address, "-t", f"python3 {settings.remote_folder}/scripts/marlin_flash.py"])
+
+
+if __name__ == "__main__":
+    main()
